7a2e8ed9e64f02d48301b385292a41a7b9866feb
Zim <lenizio27@gmail.com>
2026-03-16 20:36:26 -0300

    style: novo estilo de componente

--- back_end/server.js
+++ back_end/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -89,4 +90,4 @@ app.get('/users/:id', (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+app.listen(PORT, () => console.log(`"Servidor rodando na porta ${PORT}`));
